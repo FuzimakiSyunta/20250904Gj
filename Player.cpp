ad8f932e62bab8e5ed67d2095c088bee7dc53602
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2025-09-07 20:39:12 +0900

    玉のの動き修正

--- Player.cpp
+++ Player.cpp
@@ -64,15 +64,18 @@ void Player::Update() {
 
     // --- ボールがすべて停止している場合のみドラッグ処理 ---
    
-
+    if (ballspeed0 == true && IsStopped() == true)
+    {
         // ドラッグ開始
-        if (input_->IsTriggerMouse(0)) {
+        if (input_->IsTriggerMouse(0)) 
+        {
             dragging_ = true;
             dragStart_ = mousePos;
         }
-
+        
         // ドラッグ中
-        if (!arrowFlying_ && !arrowReturning_ && dragging_ && input_->IsPressMouse(0)) {
+        if (!arrowFlying_ && !arrowReturning_ && dragging_ && input_->IsPressMouse(0)) 
+        {
             dragCurrent_ = mousePos;
 
             Vector2 diff = { dragStart_.x - dragCurrent_.x, dragStart_.y - dragCurrent_.y };
@@ -85,7 +88,7 @@ void Player::Update() {
             Vector2 offset = { 10.0f, 30.0f };
             playerArrowSprite_->SetPosition({ pos.x + offset.x, pos.y + offset.y });
         }
-        if (ballspeed0 == true && IsStopped() == true) {
+       
         // ドラッグ終了
         if (dragging_ && !input_->IsPressMouse(0)) {
             dragging_ = false;
@@ -95,6 +98,7 @@ void Player::Update() {
             vel_.y = diff.y * power;
         }
     }
+
     // --- 摩擦 ---
     vel_ *= 0.98f;
 
@@ -130,7 +134,7 @@ void Player::Update() {
 void Player::Draw() {
     playerSprite_->Draw();
     // 左クリック押下中のみ矢印表示
-    if (input_->IsPressMouse(0) && ballspeed0 == true && IsStopped() == true) {
+    if (dragging_ == true && ballspeed0 == true && IsStopped() == true) {
         playerArrowSprite_->Draw();
     }
 
